1e152ab3751b2f4154cc65377bb9bf6fd6df1574
Will Walton <will@endless3dge.com>
2024-02-23 02:27:18 -0600

    variables set in script.js
    in the index.html changed the name tag for Duration from position to duration
    
    added the variables to the script file
    these are the variables for the resume form inputs, all of them

---
+++ script.js
@@ -0,0 +1,22 @@
+function formValidation() {
+    //setting these variables at 'var' so they can be accessed on the global scope
+    var uname = document.registration.username;
+    var uadd = document.registration.address;
+    var ucountry = document.registration.country;
+    var uzip = document.registration.zip;
+    var uemail = document.registration.email;
+    var unumber = document.registration.phonenumber;
+    var usex = document.registration.sex;
+    var ulang = document.registration.language;
+    var uabout = document.registration.desc;
+    var uinsti = document.registration.institution;
+    var udegree = document.registration.degree;
+    var uyear = document.registration.year;
+    var ures = document.registration.result;
+    var upname = document.registration.pname;
+    var upabout = document.registration.upabout;
+    var upos = document.registration.position;
+    var ucomp = document.registration.company;
+
+    
+}
